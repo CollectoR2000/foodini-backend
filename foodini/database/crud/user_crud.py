e25f5e54c0795148d686d06b88e167b1d36c9d36
CollectoR2000 <info.github@nijeboer.xyz>
2022-05-30 17:46:09 +0200

    Fixed update function to correctly hash passwords

--- foodini/database/crud/user_crud.py
+++ foodini/database/crud/user_crud.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, Union
 from sqlalchemy.orm import Session
 
 from core import crypto
-from database.mappings import CreateUserV1, StoredUserV1, UpdateUserV1
+from database.mappings import CreateUserV1, HiddenUserV1, UpdateUserV1
 from database.models import User
 
 from .base import CRUD
@@ -21,8 +21,14 @@ class UserCRUD(CRUD[User, CreateUserV1, UpdateUserV1]):
             db.commit()
         return model
 
-    def read_username(self, db: Session, username: str) -> StoredUserV1:
+    def read_username(self, db: Session, username: str) -> User:
         return db.query(self.model).filter(self.model.username.ilike(username)).first()
 
+    def update(self, db: Session, model: User, update: Union[UpdateUserV1, dict], *, commit: bool = True) -> User:
+        update = update.dict(exclude_unset=True) if not isinstance(update, dict) else update
+        password = update.pop("password")
+        update["hashed_password"] = crypto.hash_password(password)
+        return super().update(db, model, update, commit=commit)
 
-user = UserCRUD(User, StoredUserV1)
+
+user = UserCRUD(User, HiddenUserV1)
